108e5455d46c4c6bc57dddd48caed83a45b5c42e
Frazou1 <89617565+Frazou1@users.noreply.github.com>
2025-02-25 09:06:07 -0500

    Update run.sh

--- exemple/run.sh
+++ exemple/run.sh
@@ -12,20 +12,19 @@ MQTT_PORT="$(jq -r '.mqtt_port' $OPTIONS_FILE)"
 MQTT_USERNAME="$(jq -r '.mqtt_username' $OPTIONS_FILE)"
 MQTT_PASSWORD="$(jq -r '.mqtt_password' $OPTIONS_FILE)"
 
-
 echo "[INFO] Démarrage de l'add-on HA OpenDNS Updater"
 echo "[INFO] 📡 MQTT = $MQTT_HOST:$MQTT_PORT (utilisateur: $MQTT_USERNAME)"
 
 while true; do
     echo "Vérification de l'adresse IP publique..."
     python3 /script.py \
-    --mqtt_host "$USERNAME" \
-    --mqtt_host "$PASSWORD" \
-    --mqtt_host "$NETWORK_LABEL" \
-    --mqtt_host "$MQTT_HOST" \
-    --mqtt_port "$MQTT_PORT" \
-    --mqtt_username "$MQTT_USERNAME" \
-    --mqtt_password "$MQTT_PASSWORD"
+    "$USERNAME" \
+    "$PASSWORD" \
+    "$NETWORK_LABEL" \
+    "$MQTT_HOST" \
+    "$MQTT_PORT" \
+    "$MQTT_USERNAME" \
+    "$MQTT_PASSWORD"
     
     echo "Attente de $INTERVAL secondes avant la prochaine vérification."
     sleep $INTERVAL
